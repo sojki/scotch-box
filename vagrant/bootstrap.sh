bdc4f70653263d11f9fd072e4086fb1c60f2accf
Martin Sojka <sojki@seznam.cz>
2015-10-28 11:47:43 +0100

    Added provisioning, install mc and enable swap

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install MC
+sudo apt-get install mc
+
+# Enable swap
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
